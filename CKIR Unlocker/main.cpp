68ded7e2df7f537b79a77aa1163b7551f24e8867
배동인 <67633420+skyneton@users.noreply.github.com>
2025-05-01 08:28:49 +0900

    Update main.cpp

--- CKIR Unlocker/main.cpp
+++ CKIR Unlocker/main.cpp
@@ -2,19 +2,33 @@
 #include <TlHelp32.h>
 #include <iostream>
 using namespace std;
+#define INITIALIZE_IOCTL_CODE 0x9876C004
+#define TERMINSTE_PROCESS_IOCTL_CODE 0x9876C094
 
-bool terminate_proc(PROCESSENTRY32& procEntry) {
+bool TerminateProc(PROCESSENTRY32& procEntry) {
 	DWORD dwExitCode = 0;
 	auto handle = OpenProcess(PROCESS_ALL_ACCESS, 0, procEntry.th32ProcessID);
 	if (GetExitCodeProcess(handle, &dwExitCode)) {
 		if (TerminateProcess(handle, dwExitCode)) {
+			CloseHandle(handle);
 			return true;
 		}
 	}
+	CloseHandle(handle);
+
+	DWORD output[2] = { 0 };
+	DWORD size = sizeof(output);
+	DWORD byteReturned = 0;
+	handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (DeviceIoControl(handle, TERMINSTE_PROCESS_IOCTL_CODE, &procEntry.th32ProcessID, sizeof(procEntry.th32ProcessID), output, size, &byteReturned, NULL)) {
+		CloseHandle(handle);
+		return true;
+	}
+	CloseHandle(handle);
 	return false;
 }
 
-void unlock() {
+void Unlock() {
 	auto handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 	PROCESSENTRY32 procEntry = { 0 };
@@ -23,37 +37,38 @@ void unlock() {
 	do {
 		if (!wcscmp(procEntry.szExeFile, L"lqndauccd.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebSvr.exe")
 			|| !wcscmp(procEntry.szExeFile, L"qukapttp.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebAgent.exe")
-			|| !wcscmp(procEntry.szExeFile, L"nfowjxyfd.exe")) {
-			cout << "Latest CKIR process [" << procEntry.szExeFile << "] ";
-			if (terminate_proc(procEntry)) cout << "KILLED\n";
+			|| !wcscmp(procEntry.szExeFile, L"nfowjxyfd.exe") || !wcscmp(procEntry.szExeFile, L"notepad.exe")) {
+			cout << "Latest CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
+			if (TerminateProc(procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"TDepend64.exe") || !wcscmp(procEntry.szExeFile, L"TDepend.exe")) {
-			cout << "Remote control process [" << procEntry.szExeFile << "] ";
-			if (terminate_proc(procEntry)) cout << "KILLED\n";
+			cout << "Remote control process ["; wcout << procEntry.szExeFile; cout << "] ";
+			if (TerminateProc(procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"rwtyijsa.exe") || !wcscmp(procEntry.szExeFile, L"nhfneczzm.exe")) {
-			cout << "Unknown process [" << procEntry.szExeFile << "] ";
-			if (terminate_proc(procEntry)) cout << "KILLED\n";
+			cout << "Unknown process ["; wcout << procEntry.szExeFile; cout << "] ";
+			if (TerminateProc(procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"SoluSpSvr.exe") || !wcscmp(procEntry.szExeFile, L"MaestroNAgent.exe")
 			|| !wcscmp(procEntry.szExeFile, L"MaestroNSvr.exe")) {
-			cout << "Legacy CKIR process [" << procEntry.szExeFile << "] ";
-			if (terminate_proc(procEntry)) cout << "KILLED\n";
+			cout << "Legacy CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
+			if (TerminateProc(procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 	} while (Process32Next(handle, &procEntry));
+	CloseHandle(handle);
 }
 int main() {
 	cin.tie(0), cout.tie(0)->sync_with_stdio(0);
-	unlock();
-	cout << "\n\n";
-	system("PAUSE");
+	Unlock();
+	cout << "\n\nPress Enter to exit...";
+	getchar();
 	return 0;
 }
