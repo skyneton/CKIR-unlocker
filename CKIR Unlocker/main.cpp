5e140d2569dca30e87466d85b481a24b41dde203
배동인 <67633420+skyneton@users.noreply.github.com>
2025-07-13 09:15:58 +0900

    update

--- CKIR Unlocker/main.cpp
+++ CKIR Unlocker/main.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 #define INITIALIZE_IOCTL_CODE 0x9876C004
 #define TERMINSTE_PROCESS_IOCTL_CODE 0x9876C094
+#define LOOP_COUNT 20
 
 typedef long (NTAPI* pNtProcess)(HANDLE proccessHandle);
 pNtProcess NtSuspendProcess;
@@ -22,188 +23,32 @@ static bool LoadNT() {
     return !!NtSuspendProcess;
 }
 
-static bool LoadDriver(const char* serviceName, char* driverPath) {
-    SC_HANDLE hSCM, hService;
-
-    // Open a handle to the SCM database
-    hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-    if (hSCM == NULL) {
-        return 0;
-    }
-
-    // Check if the service already exists
-    hService = OpenServiceA(hSCM, serviceName, SERVICE_ALL_ACCESS);
-    if (hService != NULL)
-    {
-        cout << "Service already exists.\n";
-
-        // Start the service if it's not running
-        SERVICE_STATUS serviceStatus;
-        if (!QueryServiceStatus(hService, &serviceStatus))
-        {
-            CloseServiceHandle(hService);
-            CloseServiceHandle(hSCM);
-            return 0;
-        }
-
-        if (serviceStatus.dwCurrentState == SERVICE_STOPPED)
-        {
-            if (!StartServiceA(hService, 0, nullptr))
-            {
-                cout << "Could not found.\n";
-                CloseServiceHandle(hService);
-                CloseServiceHandle(hSCM);
-                return 0;
-            }
-
-            cout << "Starting service...\n";
-        }
-
-        CloseServiceHandle(hService);
-        CloseServiceHandle(hSCM);
-        return 1;
-    }
-
-    // Create the service
-    hService = CreateServiceA(
-        hSCM,
-        serviceName,
-        serviceName,
-        SERVICE_ALL_ACCESS,
-        SERVICE_KERNEL_DRIVER,
-        SERVICE_DEMAND_START,
-        SERVICE_ERROR_IGNORE,
-        driverPath,
-        NULL,
-        NULL,
-        NULL,
-        NULL,
-        NULL
-    );
-
-    if (hService == NULL) {
-        CloseServiceHandle(hSCM);
-        return (1);
-    }
-
-    cout << "Service created successfully.\n";
-
-    // Start the service
-    if (!StartServiceA(hService, 0, nullptr))
-    {
-        CloseServiceHandle(hService);
-        CloseServiceHandle(hSCM);
-        return 0;
-    }
-
-    cout << "Starting service...\n";
-
-    CloseServiceHandle(hService);
-    CloseServiceHandle(hSCM);
-
-    return 1;
-}
-
-static bool RemoveDriver(const char* serviceName, char* driverPath) {
-    SC_HANDLE hSCM, hService;
-
-    // Open a handle to the SCM database
-    hSCM = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-    if (hSCM == NULL) {
-        return 0;
-    }
-
-    // Check if the service already exists
-    hService = OpenServiceA(hSCM, serviceName, SERVICE_ALL_ACCESS);
-    if (hService == NULL)
-    {
-        cout << "Service already deleted.\n";
-        return 0;
-    }
-    DeleteService(hService);
-
-    cout << "Service deleted.\n";
-
-    CloseServiceHandle(hService);
-    CloseServiceHandle(hSCM);
-
-    return 1;
-}
-
-class TerminateData {
-public:
-    DWORD pid;
-    bool terminated;
-    TerminateData(DWORD pid) :pid(pid), terminated(false) {}
-};
-
-static BOOL CALLBACK TerminateByHWND(HWND hwnd, LPARAM lParam) {
-    DWORD pid;
-    TerminateData* data = reinterpret_cast<TerminateData*>(lParam);
-    GetWindowThreadProcessId(hwnd, &pid);
-    if (pid != data->pid) return true;
-    if (!ChangeWindowMessageFilterEx(hwnd, WM_CLOSE, MSGFLT_ALLOW, 0)) {
-        cout << '(' << std::hex << GetLastError() << ')';
-    }
-    if (PostMessageA(hwnd, WM_CLOSE, NULL, NULL)) {
-        data->terminated = true;
-    }
-    return true;
-}
-
-static bool TerminateProc(HANDLE terminateHandle, PROCESSENTRY32& procEntry, int buildType) {
+static int TerminateProc(PROCESSENTRY32& procEntry, int buildType) {
     DWORD output[2] = { 0 };
     DWORD size = sizeof(output);
     DWORD byteReturned = 0;
-    if (DeviceIoControl(terminateHandle, TERMINSTE_PROCESS_IOCTL_CODE, &procEntry.th32ProcessID, sizeof(procEntry.th32ProcessID), output, size, &byteReturned, NULL)) {
-        return true;
-    }
 
 	DWORD dwExitCode = 0;
-	auto handle = OpenProcess(PROCESS_ALL_ACCESS, true, procEntry.th32ProcessID);
-    cout << procEntry.th32ProcessID << '\n';
+	auto handle = OpenProcess(PROCESS_ALL_ACCESS, false, procEntry.th32ProcessID);
     TCHAR filePath[MAX_PATH] = { 0 };
     DWORD len = MAX_PATH;
     QueryFullProcessImageNameW(handle, NULL, filePath, &len);
-    if (InjectCode(handle)) {
-        cout << "\n* Injection Success\n";
-        if (len != MAX_PATH) {
-            DeleteFileW(filePath);
-            cout << '[';
-            wcout << filePath;
-            cout << ']';
+    if (GetExitCodeProcess(handle, &dwExitCode)) {
+        if (TerminateProcess(handle, dwExitCode)) {
+            CloseHandle(handle);
+            return 1;
         }
-        return true;
     }
-	if (GetExitCodeProcess(handle, &dwExitCode)) {
-		if (TerminateProcess(handle, dwExitCode)) {
-			CloseHandle(handle);
-			return true;
-		}
-	}
-
     if (buildType == BUILD_TYPE_FORCE) {
-        TerminateData result(procEntry.th32ProcessID);
-        EnumWindows(TerminateByHWND, reinterpret_cast<long long>(&result));
-        if (len != MAX_PATH) {
-            DeleteFileW(filePath);
-            cout << '[';
-            wcout << filePath;
-            cout << ']';
-        }
-        if (result.terminated) {
-            if (WaitForSingleObject(handle, 300) != WAIT_OBJECT_0) {
-                if (TerminateProcess(handle, dwExitCode)) {
-                    CloseHandle(handle);
-                    return true;
-                }
+        if (InjectCode(handle)) {
+            cout << "\n* Injection Success\n";
+            if (len != MAX_PATH && DeleteFileW(filePath)) {
+                cout << '[';
+                wcout << filePath;
+                cout << ']';
             }
-            else {
-                CloseHandle(handle);
-                return true;
-            }
+            return 1;
         }
-        cout << "Force Terminate Failed(" << std::hex << GetLastError() << ")";
     }
 
     if (NtSuspendProcess) {
@@ -211,15 +56,15 @@ static bool TerminateProc(HANDLE terminateHandle, PROCESSENTRY32& procEntry, int
         cout << result;
         if (result >= 0) {
             CloseHandle(handle);
-            return true;
+            return 2;
         }
     }
 
 	CloseHandle(handle);
-	return false;
+	return 0;
 }
 
-static bool ResumeProc(HANDLE resumeHandle, PROCESSENTRY32& procEntry) {
+static bool ResumeProc(PROCESSENTRY32& procEntry) {
     auto handle = OpenProcess(PROCESS_ALL_ACCESS, true, procEntry.th32ProcessID);
 
     if (NtSuspendProcess) {
@@ -235,83 +80,96 @@ static bool ResumeProc(HANDLE resumeHandle, PROCESSENTRY32& procEntry) {
     return false;
 }
 
-static void Unlock(int buildType) {
-    auto terminateHandle = CreateFile(L"\\\\.\\Blackout", GENERIC_WRITE | GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    /*if (terminateHandle == INVALID_HANDLE_VALUE) {
-        cout << "INVALID HANDLE\n";
-    }*/
-	auto handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+static PROCESSENTRY32 Find(const wchar_t* name) {
+    auto handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-	PROCESSENTRY32 procEntry = { 0 };
-	procEntry.dwSize = sizeof(PROCESSENTRY32);
-	Process32First(handle, &procEntry);
-	do {
-		if (!wcscmp(procEntry.szExeFile, L"lqndauccd.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebSvr.exe")
-			|| !wcscmp(procEntry.szExeFile, L"qukapttp.exe") || !wcscmp(procEntry.szExeFile, L"MaestroWebAgent.exe")
-            || !wcscmp(procEntry.szExeFile, L"nfowjxyfd.exe")) {
-			cout << "Latest CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
-			if (buildType == BUILD_TYPE_RESUME && ResumeProc(terminateHandle, procEntry)) cout << "RESUME\n";
-            else if (TerminateProc(terminateHandle, procEntry, buildType)) cout << "KILLED\n";
-			else cout << "KILL failed\n";
-			continue;
-		}
-		if (!wcscmp(procEntry.szExeFile, L"TDepend64.exe") || !wcscmp(procEntry.szExeFile, L"TDepend.exe")) {
-			cout << "Remote control process ["; wcout << procEntry.szExeFile; cout << "] ";
-            if (buildType == BUILD_TYPE_RESUME && ResumeProc(terminateHandle, procEntry)) cout << "RESUME\n";
-            else if (TerminateProc(terminateHandle, procEntry, buildType)) cout << "KILLED\n";
-			else cout << "KILL failed\n";
-			continue;
-		}
-		if (!wcscmp(procEntry.szExeFile, L"rwtyijsa.exe") || !wcscmp(procEntry.szExeFile, L"nhfneczzm.exe")) {
-			cout << "Unknown process ["; wcout << procEntry.szExeFile; cout << "] ";
-            if (buildType == BUILD_TYPE_RESUME && ResumeProc(terminateHandle, procEntry)) cout << "RESUME\n";
-            else if (TerminateProc(terminateHandle, procEntry, buildType)) cout << "KILLED\n";
-			else cout << "KILL failed\n";
-			continue;
-		}
-		if (!wcscmp(procEntry.szExeFile, L"SoluSpSvr.exe") || !wcscmp(procEntry.szExeFile, L"MaestroNAgent.exe")
-			|| !wcscmp(procEntry.szExeFile, L"MaestroNSvr.exe")) {
-			cout << "Legacy CKIR process ["; wcout << procEntry.szExeFile; cout << "] ";
-            if (buildType == BUILD_TYPE_RESUME && ResumeProc(terminateHandle, procEntry)) cout << "RESUME\n";
-            else if (TerminateProc(terminateHandle, procEntry, buildType)) cout << "KILLED\n";
-			else cout << "KILL failed\n";
-			continue;
-		}
-	} while (Process32Next(handle, &procEntry));
-	CloseHandle(handle);
-    CloseHandle(terminateHandle);
+    PROCESSENTRY32 result = { 0 };
+    PROCESSENTRY32 procEntry = { 0 };
+    result.th32ProcessID = 0;
+    procEntry.dwSize = sizeof(PROCESSENTRY32);
+    Process32First(handle, &procEntry);
+    do {
+        if (!wcscmp(procEntry.szExeFile, name)) continue;
+        result = procEntry;
+        break;
+    } while (Process32Next(handle, &procEntry));
+    CloseHandle(handle);
+    return result;
+}
+
+static void LoopKiller(const char* type, int count, const wchar_t* list[], int length, int buildType) {
+    cout << type << " Process Detecting...\n";
+    for (int i = 0; i < length; i++) {
+        auto p = Find(list[i]);
+        if (p.th32ProcessID == 0) continue;
+        cout << "Detected ["; wcout << p.szExeFile; cout << "](PID:" << p.th32ProcessID << ")\n";
+        auto handle = OpenProcess(PROCESS_ALL_ACCESS, false, p.th32ProcessID);
+        wchar_t filePath[MAX_PATH] = { 0 };
+        DWORD len = MAX_PATH;
+        QueryFullProcessImageNameW(handle, NULL, filePath, &len);
+        CloseHandle(handle);
+        cout << "PATH: "; wcout << filePath; cout << '\n';
+        if (buildType == BUILD_TYPE_RESUME) {
+            cout << "Try To Resume...\n";
+            if (ResumeProc(p)) cout << "Success!\n";
+            else cout << "Failed.";
+            continue;
+        }
+        bool killed = false;
+        bool deleted = false;
+        cout << "Try To Kill...\n";
+
+        for (int j = 0; j < count; j++) {
+            p = Find(list[i]);
+            if (!p.th32ProcessID) {
+                killed = 1;
+                break;
+            }
+
+            int result = TerminateProc(p, buildType);
+            if (!result) continue;
+            if (result == 2) {
+                cout << "Suspended..\n";
+                break;
+            }
+            if (length != MAX_PATH);
+            if (DeleteFileW(filePath)) {
+                deleted = true;
+                killed = true;
+                break;
+            }
+        }
+
+        if (!killed) {
+            p = Find(list[i]);
+            if (!p.th32ProcessID) killed = true;
+        }
+        cout << "Kill " << (killed ? "Success" : "Failed") << "...\n";
+        cout << "Try To Delete Execution File...\n";
+        if (deleted) cout << "Success!\n";
+        else cout << "Failed.\n";
+    }
 }
 
 void run(int buildType) {
     cin.tie(0), cout.tie(0)->sync_with_stdio(0);
     wcin.tie(0), wcout.tie(0)->sync_with_stdio(0);
     if (Privilege()) cout << "Success Privileges.\n";
-
-    WIN32_FIND_DATAA file;
-    char driverPath[MAX_PATH];
-    if (FindFirstFileA("Blackout.sys", &file) != INVALID_HANDLE_VALUE) {
-        if (GetFullPathNameA(file.cFileName, MAX_PATH, driverPath, NULL) != 0) {
-            cout << "Driver found.\n";
-        }
-        else
-            cout << "Find Driver Failed\n";
-    }
-    else
-        cout << "Find Driver Failed\n";
-
-    if (!LoadDriver("Blackout", driverPath))
-        cout << "Faild to load driver ,try to run the program as administrator!!\n";
+    const wchar_t* latest[] = { L"lqndauccd.exe", L"MaestroWebSvr.exe", L"qukapttp.exe", L"MaestroWebAgent.exe", L"nfowjxyfd.exe" };
+    const wchar_t* remote[] = { L"TDepend64.exe", L"TDepend.exe" };
+    const wchar_t* unknown[] = { L"rwtyijsa.exe", L"nhfneczzm.exe" };
+    const wchar_t* legacy[] = { L"SoluSpSvr.exe", L"MaestroNAgent.exe", L"MaestroNSvr.exe" };
 
     if (LoadNT())
         cout << "NT Loaded\n";
     else
         cout << "NT Load Failed..\n";
 
-	Unlock(buildType);
-    end:
-    if (!RemoveDriver("Blackout", driverPath)) {
-        cout << "Service delete failed.\n";
-    }
+    LoopKiller("Latest CKIR", LOOP_COUNT, latest, sizeof(latest) / sizeof(const wchar_t*), buildType);
+    LoopKiller("Remote Control", LOOP_COUNT, remote, sizeof(remote) / sizeof(const wchar_t*), buildType);
+    LoopKiller("Unknown", LOOP_COUNT, unknown, sizeof(unknown) / sizeof(const wchar_t*), buildType);
+    LoopKiller("Legacy", LOOP_COUNT, legacy, sizeof(legacy) / sizeof(const wchar_t*), buildType);
+
 	cout << "\n\nPress Enter to exit...";
     system("pause");
 	getchar();
